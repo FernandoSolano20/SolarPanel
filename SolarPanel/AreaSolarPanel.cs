426f7e4464c738a342a5568db407e293266297d4
Fernando Solano <fesolano@ex2outcoding.local>
2019-03-15 13:53:11 -0600

    Add project files.

---
+++ SolarPanel/AreaSolarPanel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarPanel
+{
+    internal static class AreaSolarPanel
+    {
+        public static IList<int> answer(int area)
+        {
+            int sideInteger;
+            double sideDouble;
+            int _areaSmallsquare;
+            List<int> _areasSquares = new List<int>();
+
+            while (area != 0)
+            {
+                sideDouble = Math.Sqrt(area);
+                sideInteger = (int)Math.Floor(sideDouble);
+                _areaSmallsquare = sideInteger * sideInteger;
+                _areasSquares.Add(_areaSmallsquare);
+                area -= _areaSmallsquare;
+            }
+
+            return _areasSquares;
+        }
+    }
+}
---
+++ SolarPanel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarPanel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] areas;
+            areas = AreaSolarPanel.answer(15324).ToArray();
+
+            foreach (int area in areas)
+            {
+                Console.WriteLine(area);
+            }
+        }
+    }
+}
